4fce28c2342911a3512254ebd50373999b20795c
unknown <manhha2392000@gmail.com>
2023-04-08 09:25:38 +0700

    dm hung

--- routes/userRouter.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const userController = require("../controllers/userController")
-
-router.post("/register",userController.register);
-router.post("/login",userController.login);
-router.put("/:id", userController.updateUser);
-
-module.exports = router;
