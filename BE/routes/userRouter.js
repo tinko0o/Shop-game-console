bcec4b538b67412d0463694eb131133daf6fa9dc
unknown <manhha2392000@gmail.com>
2023-04-10 14:37:08 +0700

    show user information

--- BE/routes/userRouter.js
+++ BE/routes/userRouter.js
@@ -4,5 +4,6 @@ const userController = require("../controllers/userController")
 router.post("/register",userController.register);
 router.post("/login",userController.login);
 router.put("/:id", userController.updateUser);
+router.get("/:id",userController.showInformation);
 
 module.exports = router;
