26eb97e0efaedbb7e07a3a5b5f16adb28abadde1
unknown <manhha2392000@gmail.com>
2023-04-11 16:32:28 +0700

    get all user, delete user

--- BE/controllers/userController.js
+++ BE/controllers/userController.js
@@ -89,7 +89,7 @@ exports.changePassword = async (req, res) => {
   try {
     const token = req.headers.authentication;
     if (!token) {
-      return res.status(200).json({
+      return res.status(401).json({
         success: false,
         message: "Unauthorization",
       });
@@ -98,34 +98,41 @@ exports.changePassword = async (req, res) => {
     const decoded = jwt.verify(token, key);
     const { password, newPassword, confirmPassword } = req.body;
     if (!password || !newPassword || !confirmPassword) {
-      return res.status(500).json({
+      return res.status(400).json({
         success: false,
         message: "Invalid Input",
       });
     }
     const user = await User.findOne({ email: decoded.email });
     if (!user) {
-      return res.status(500).json({
+      return res.status(404).json({
         success: false,
         message: "Invalid User",
       });
     }
-    const isMatch = bcrypt.compareSync(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(500).json({
+      return res.status(400).json({
         success: false,
-        message: "invalid password",
+        message: "Invalid password",
       });
     }
-    const salt = await bcrypt.genSaltSync(12);
-    hashedPassword = await bcrypt.hashSync(newPassword, salt);
-    await User.updateOne({ password: hashedPassword });
+    if (newPassword !== confirmPassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Passwords do not match",
+      });
+    }
+    const salt = await bcrypt.genSalt(12);
+    const hashedPassword = await bcrypt.hash(newPassword, salt);
+    await User.updateOne({ email: decoded.email }, { password: hashedPassword });
     res.status(200).json({
       success: true,
-      message: "Success"
+      message: "Password changed successfully"
     });
   } catch (err) {
-    res.status(500).json({ success: false, state: "Some thing is wrong" });
+    console.log(err);
+    res.status(500).json({ success: false, message: "Something went wrong" });
   }
 };
 
@@ -135,31 +142,134 @@ exports.updateUser = async (req, res) => {
   try {
     const token = req.headers.authentication;
     if (!token) {
-      return res.status(200).json({
+      return res.status(401).json({
         success: false,
-        message: "Unauthorization",
+        message: "Unauthorized",
       });
     }
     const key = process.env.JWT_SEC;
     const decoded = jwt.verify(token, key);
+    const user = await User.findOne({ email: decoded.email })
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
     await User.findByIdAndUpdate(req.params.id, req.body);
     res.status(200).json({
       success: true,
-      message: "Success"
+      message: "User updated successfully",
     });
   } catch (error) {
-    console.log(JSON.stringify(error, null, 2));
-    console.log();
-    if (error.code === 11000) {
-      res.status(400).json({
-        success: false,
-        message: `${Object.keys(error.keyValue)} is already existed`,
-      });
-    }
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+    });
   }
 };
 
 //delete user
 
+exports.deleteUser = async (req, res) => {
+  try {
+    const token = req.headers.authentication;
+    if (!token) {
+      res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+    const key = process.env.JWT_SEC;
+    const decoded = jwt.verify(token, key);
+    const user = await User.findOne({ email: decoded.email })
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Invalid user",
+      });
+    }
+    if (!user.isAdmin) {
+      return res.status(403).json({
+        success: false,
+        message: "Forbidden",
+      });
+    }
+    await User.findByIdAndRemove(req.params.id);
+    res.status(200).json({ success: true, message: "Success" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "An error occurred while deleting user" });
+  }
+};
 
+//get all users
 
+exports.getAllUsers = async (req, res) => {
+  try {
+    const token = req.headers.authentication;
+    if (!token) {
+      res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+    const key = process.env.JWT_SEC;
+    const decoded = jwt.verify(token, key);
+    const user = await User.findOne({ email: decoded.email });
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Invalid user",
+      });
+    }
+    if (!user.isAdmin) {
+      return res.status(403).json({
+        success: false,
+        message: "Forbidden",
+      });
+    }
+    const users = await User.find({ isAdmin: false });
+    res.status(200).json({
+      success: true,
+      users,
+    });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Something went wrong" });
+  }
+};
+
+//get all admins
+
+exports.getAllAdmins = async (req, res) => {
+  try {
+    const token = req.headers.authentication;
+    if (!token) {
+      res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+    const key = process.env.JWT_SEC;
+    const decoded = jwt.verify(token, key);
+    const user = await User.findOne({ email: decoded.email });
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Invalid user",
+      });
+    }
+    if (!user.isAdmin) {
+      return res.status(403).json({
+        success: false,
+        message: "Forbidden",
+      });
+    }
+    const users = await User.find({ isAdmin: true });
+    res.status(200).json({
+      success: true,
+      users,
+    });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Something went wrong" });
+  }
+};
--- BE/routes/userRouter.js
+++ BE/routes/userRouter.js
@@ -5,5 +5,8 @@ router.post("/register",userController.register);
 router.post("/login",userController.login);
 router.put("/changepassword/:id",userController.changePassword);
 router.put("/update/:id", userController.updateUser);
+router.delete("/delete/:id", userController.deleteUser);
+router.get("/users",userController.getAllUsers);
+router.get("/admins",userController.getAllAdmins);
 
 module.exports = router;
