a520f2c5bf1b43c7e812c12adb47c28879d7664d
TrinityOxo <manhha2392000@gmail.com>
2023-05-19 16:39:53 +0700

    nothing :)

--- BE/controllers/ratingController.js
+++ BE/controllers/ratingController.js
@@ -92,10 +92,7 @@ exports.addRating = async (req, res) => {
         (u) => u.id.toString() === user.id.toString()
       );
       if (userRatingIndex >= 0) {
-        return res.status(400).json({
-          success: false,
-          message: "You have already rated this product in the current order",
-        });
+        existingRating.users[userRatingIndex].rating = rating;
       } else {
         existingRating.users.push({
           id: user.id,
@@ -125,126 +122,6 @@ exports.addRating = async (req, res) => {
     res.status(500).json({ success: false, message: "Something went wrong" });
   }
 };
-
-
-
-
-
-
-// exports.addRating = async (req, res) => {
-//   try {
-//     const token = req.headers.authentication;
-//     if (!token) {
-//       return res.status(401).json({
-//         success: false,
-//         message: "Unauthorized",
-//       });
-//     }
-//     const key = process.env.JWT_SEC;
-//     const decoded = jwt.verify(token, key);
-//     const user = await User.findOne({ email: decoded.email });
-//     if (!user) {
-//       return res.status(401).json({
-//         success: false,
-//         message: 'User not found',
-//       });
-//     }
-
-//     const product = await Product.findById(req.body.productId);
-//     if (!product) {
-//       return res.status(404).json({
-//         success: false,
-//         message: 'Product not found',
-//       });
-//     }
-
-//     const order = await Order.findOne({
-//       _id: req.body.orderId,
-//       userId: user.id,
-//       "products.id": req.body.productId
-//     }).sort({ createdAt: -1 });
-
-//     if (!order) {
-//       return res.status(400).json({
-//         success: false,
-//         message: "You can only rate products that you have ordered",
-//       });
-//     }
-
-//     if (order.status !== "Đã giao") {
-//       return res.status(400).json({
-//         success: false,
-//         message: "You can only rate delivered products",
-//       });
-//     }
-
-//     const rating = req.body.rating;
-//     if (rating < 1 || rating > 5) {
-//       return res.status(400).json({
-//         success: false,
-//         message: "Rating must be from 1 to 5",
-//       });
-//     }
-
-//     const existingRating = await Rating.findOne({ productId: req.body.productId });
-//     if (!existingRating) {
-//       const newRating = new Rating({
-//         productId: req.body.productId,
-//         users: [
-//           {
-//             id: user.id,
-//             name: user.name,
-//             email: user.email,
-//             rating: rating,
-//           },
-//         ],
-//         avgRating: rating,
-//         totalRating: 1,
-//       });
-//       await newRating.save();
-//       const orders = await Order.updateMany(
-//         { userId: user.id, "products.id": req.body.productId },
-//         { $set: { "products.$.rating": req.body.rating } }
-//       );
-//       return res.status(200).json({
-//         success: true,
-//         data: newRating,
-//       });
-//     } else {
-//       const userRatingIndex = existingRating.users.findIndex(
-//         (u) => u.id.toString() === user.id.toString()
-//       );
-//       if (userRatingIndex >= 0) {
-//         existingRating.users[userRatingIndex].rating = rating;
-//       } else {
-//         existingRating.users.push({
-//           id: user.id,
-//           name: user.name,
-//           email: user.email,
-//           rating: rating,
-//         });
-//       }
-//       const sumOfRatings = existingRating.users.reduce((acc, cur) => acc + cur.rating, 0);
-//       const totalRating = existingRating.users.length;
-//       const roundedAvgRating = Math.round((sumOfRatings / totalRating) * 2) / 2;
-//       existingRating.avgRating = roundedAvgRating;
-//       existingRating.totalRating = totalRating;
-//       await existingRating.save();
-
-//       const orders = await Order.updateMany(
-//         { userId: user.id, "products.id": req.body.productId },
-//         { $set: { "products.$.rating": req.body.rating } }
-//       );
-//       return res.status(200).json({
-//         success: true,
-//         data: await Rating.findOne({ productId: req.body.productId }),
-//       });
-//     }
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ success: false, message: "Something went wrong" });
-//   }
-// };
 
 
 
